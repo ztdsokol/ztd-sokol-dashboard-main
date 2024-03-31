1ec3987ede20e77b4eb6c4f9f86ee4e721a2ff70
ztdsokol <ztdsokol@gmail.com>
2024-03-31 18:16:05 +0200

    new

--- app/(dashboard)/grupe/[grupaId]/components/group-form.tsx
+++ app/(dashboard)/grupe/[grupaId]/components/group-form.tsx
@@ -37,7 +37,7 @@ const formSchema = z.object({
   locationId: z.string().min(1),
   programId: z.string().min(1),
   members: z.array(z.string()),
-  trainers: z.array(z.string()),
+  //trainers: z.array(z.string()),
   price: z.coerce.number().min(0),
 });
 
@@ -48,7 +48,7 @@ interface GroupFormProps {
   programs: Program[];
   locations: Location[];
   members?: Member[];
-  trainers?: Member[];
+  //trainers?: Member[];
 }
 
 export const GroupForm: React.FC<GroupFormProps> = ({
@@ -56,7 +56,7 @@ export const GroupForm: React.FC<GroupFormProps> = ({
   programs,
   locations,
   members: allMembers,
-  trainers: allTrainers,
+  //trainers: allTrainers,
 }) => {
   const params = useParams();
   const router = useRouter();
@@ -79,7 +79,7 @@ export const GroupForm: React.FC<GroupFormProps> = ({
       programId: "",
       price: 0,
       members: [],
-      trainers: [],
+      //trainers: [],
     },
   });
   const onMemberChange = (members: Member[]) => {
@@ -116,7 +116,7 @@ export const GroupForm: React.FC<GroupFormProps> = ({
         locationId: data.locationId,
         programId: data.programId,
         members: data.members.map((memberId: string) => ({ memberId })),
-        trainers: data.trainers.map((trainerId: string) => ({ trainerId })),
+        //trainers: data.trainers.map((trainerId: string) => ({ trainerId })),
         id: params?.grupaId,
       };
       setLoading(true);
@@ -355,39 +355,7 @@ export const GroupForm: React.FC<GroupFormProps> = ({
             </div>
             <div className="flex flex-col gap-3">
               <div className="rounded-xl border bg-card text-card-foreground shadow">
-                <div className="flex flex-col justify-center  p-4 gap-12">
-                  <FormField
-                    control={form.control}
-                    name="trainers"
-                    render={({ field }) => (
-                      <FormItem className="flex flex-row items-center h-4">
-                        <FormLabel className="text-sm text-muted-foreground min-w-[133px] pt-2">
-                          Članovi
-                        </FormLabel>
-                        <FormControl>
-                          <MultiSelectMember
-                            placeholder="ccc"
-                            members={members}
-                            loading={loading}
-                            action={action}
-                            selected={selected}
-                            handleFinalSelection={handleFinalSelection}
-                            setSelected={setSelected}
-                            selectedMembers={members.filter(
-                              (member) =>
-                                (initialData as any)?.members?.find(
-                                  (selectedMember: any) =>
-                                    selectedMember.memberId === member.id
-                                )!!
-                            )}
-                            onMemberChange={onMemberChange}
-                          />
-                        </FormControl>
-                        <FormMessage className="text-red-1" />
-                      </FormItem>
-                    )}
-                  />
-                </div>
+                <div className="flex flex-col justify-center  p-4 gap-12"></div>
               </div>{" "}
             </div>
           </div>
